70b9376d33578c488c555cc901f36dee67658271
liuyubor <liuyubor@gmail.com>
2023-03-14 01:19:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,17 @@ module.exports = {
 			iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
 			symbolId: '[name]',
 		})
-	]
+	],
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				}
+			}
+		}
+	}
 }
